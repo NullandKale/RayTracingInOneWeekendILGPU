7eada93fb392438aa5b6c9d95afc0883e18f389a
Alec ZInsli <alec.zinsli@lookingglassfactory.com>
2021-09-13 20:09:10 -0400

    Add project files.

---
+++ RayTracingInOneWeekendILGPU/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ILGPU;
+using ILGPU.Runtime;
+
+namespace RayTracingInOneWeekendILGPU
+{
+    class Program
+    {
+        static bool DEBUG = false;
+        static void Main(string[] args)
+        {
+            //Initialize ILGPU
+            Context context = Context.Create(builder => builder.Default().EnableAlgorithms());
+            Accelerator device = context.GetPreferredDevice(preferCPU: DEBUG)
+                                      .CreateAccelerator(context);
+
+            device.PrintInformation();
+        }
+    }
+}
